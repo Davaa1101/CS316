fd211d3029f24eadcd1e99cc4faaf0e8e76cc302
Davaa1101 <Davaadavka3006@gmail.com>
2026-06-01 20:15:39 +0800

    Require MONGODB_URI for database connection

--- web-project/server.js
+++ web-project/server.js
@@ -82,7 +82,7 @@ if (fs.existsSync(frontendBuildPath)) {
 }
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/barter-platform')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
